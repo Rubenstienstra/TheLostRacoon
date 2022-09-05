75d5a8bf6d049d28fe0fbf473f80be5b250c8475
Rubenstienstra <78020104+Rubenstienstra@users.noreply.github.com>
2022-09-05 15:49:29 +0200

    unity settings update

---
+++ Assets/Scripts/Ruben's Scripts/MouseTrackerMovement.cs
@@ -0,0 +1,16 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.InputSystem;
+public class MouseTrackerMovement : MonoBehaviour
+{
+    void Start()
+    {
+        
+    }
+
+    void Update()
+    {
+        
+    }
+}
