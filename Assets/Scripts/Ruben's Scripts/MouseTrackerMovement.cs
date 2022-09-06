d0f3dbace0d129ab7b1278db50568c3e9a5087e8
Rubenstienstra <78020104+Rubenstienstra@users.noreply.github.com>
2022-09-06 16:23:34 +0200

    1st methode werkt met mouse movement
    2nd methode werkt nog niet echt

--- Assets/Scripts/Ruben's Scripts/MouseTrackerMovement.cs
+++ Assets/Scripts/Ruben's Scripts/MouseTrackerMovement.cs
@@ -2,21 +2,68 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.InputSystem;
+using UnityEngine.UI;
 public class MouseTrackerMovement : MonoBehaviour
 {
-    public float mousePos;
-    public float mousePosTest;
+    public PlayerScript playerInfo;
+    public ScriptableSaving savingInfo;
+
+    public float mousePosX;
+    public float mousePosY;
+    public Level mission;
+
+    public bool mouseInZone;
+    
     void Start()
     {
+        mission.next = savingInfo.mouseTrackTimesDone;
+    }
+    public void OnMouseX(InputValue value)
+    {
+        if (playerInfo.minigameActiveMouse == true)
+        {
+            mousePosX = value.Get<float>();
+            CheckMouseTracker();
+            
+        }
         
     }
+    public void OnMouseY(InputValue value)
+    {
+        if (playerInfo.minigameActiveMouse == true)
+        {
+            mousePosY = value.Get<float>();
+            CheckMouseTracker();
 
-    void Update()
-    {
-        
+        }
+
     }
-    public void OnMouse(InputValue value)
+    public void CheckMouseTracker()
     {
-        mousePos = value.Get<float>();
+        if (mousePosX >= mission.requirementLeftX[mission.next] && mousePosX <= mission.requirementRightX[mission.next] && mousePosY <= mission.requirementUpY[mission.next] && mousePosY >= mission.requirementDownY[mission.next])
+        {
+            mouseInZone = true;
+        }
+        else
+        {
+            mouseInZone = false;
+        }
     }
+    public void InsideRadius()
+    {
+        mouseInZone = true;
+    }
+    public void OutsideRadius()
+    {
+        mouseInZone = false;
+    }
+}
+[System.Serializable]
+public class Level
+{
+    public int next;
+    public int[] requirementRightX;
+    public int[] requirementLeftX;
+    public int[] requirementUpY;
+    public int[] requirementDownY;
 }
---
+++ Assets/Scripts/Ruben's Scripts/PlayerScript.cs
@@ -0,0 +1,20 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerScript : MonoBehaviour
+{
+    public MouseTrackerMovement mouseInfo;
+    public bool minigameActiveMouse;
+    
+    void Start()
+    {
+       // minigameActiveMouse = true;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
