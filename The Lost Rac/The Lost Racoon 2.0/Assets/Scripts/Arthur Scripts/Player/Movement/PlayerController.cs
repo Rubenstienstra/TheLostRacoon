985c47bf7209b66d30db7d457c94a5a5a905bae4
arthur-lava <97080076@st.deltion.nl>
2022-09-07 15:42:53 +0200

    camera werkt nu, nu alleen nog de movement fixen

--- The Lost Rac/The Lost Racoon 2.0/Assets/Scripts/Arthur Scripts/Player/Movement/PlayerController.cs
+++ The Lost Rac/The Lost Racoon 2.0/Assets/Scripts/Arthur Scripts/Player/Movement/PlayerController.cs
@@ -6,20 +6,12 @@ using UnityEngine.InputSystem;
 public class PlayerController : MonoBehaviour {
     [Header("changeable variables", order = 0)]
     public int speed;
-    [Header("Debug variables", order = 1)]
-    public float cameraPitch = 0.0f;
-    [Header("Config variables", order = 2)]
-    public int camSensitivity;
-    public Transform cam;
-    public Rigidbody rb;
     //Input
     public PlayerInput playerInput;
     public InputAction moveAction;
     // Update is called once per frame
     private void Start() {
         moveAction = playerInput.actions["WASD"];
-        cameraPitch = 0f;
-        Cursor.lockState = CursorLockMode.Locked;
     }
     void Update() {
 
@@ -31,13 +23,6 @@ public class PlayerController : MonoBehaviour {
         float h = new float();
         float j = new float();
 
-
-        if (move.magnitude >= 0.1f) {
-            float targetAngle = Mathf.Atan2(move.x, move.z) * Mathf.Rad2Deg;
-            transform.rotation = Quaternion.Euler(0f, targetAngle, 0f);
-
-            transform.Translate(move * Time.deltaTime * speed);
-            
-        }
+        transform.Translate(move * Time.deltaTime * speed);
     }
 }
--- The Lost Rac/The Lost Racoon 2.0/Assets/Scripts/Arthur Scripts/Player/Movement/ThirdPersonCam.cs
+++ The Lost Rac/The Lost Racoon 2.0/Assets/Scripts/Arthur Scripts/Player/Movement/ThirdPersonCam.cs
@@ -3,7 +3,7 @@ using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.InputSystem;
 
-public class NewBehaviourScript : MonoBehaviour
+public class ThirdPersonCam : MonoBehaviour
 {
     [Header("References", order = 0)]
     public Transform orientation;
@@ -15,11 +15,18 @@ public class NewBehaviourScript : MonoBehaviour
     [Header("Debug", order = 1)]
     public float rotationSpeed;
 
-
+    private void Start() {
+        Cursor.lockState = CursorLockMode.Locked;
+        Cursor.visible = false;
+    }
     private void Update() {
         Vector3 viewDir = player.position - new Vector3(transform.position.x, transform.position.y, transform.position.z);
         orientation.forward = viewDir.normalized;
 
-        
+        Vector2 input = moveAction.ReadValue<Vector2>();
+        Vector3 inputDir = orientation.forward * input.x + orientation.right * input.y;
+        if(inputDir != Vector3.zero) {
+            playerObj.forward = Vector3.Slerp(playerObj.forward, inputDir.normalized, Time.deltaTime * rotationSpeed);
+        }
     }
 }
