8053c750739f965d95b8f52dc7082b36e2a596f5
arthur-lava <97080076@st.deltion.nl>
2022-09-08 14:49:10 +0200

    Movement en camera af :)

--- The Lost Rac/The Lost Racoon 2.0/Assets/Scripts/Arthur Scripts/Player/Movement/PlayerController.cs
+++ The Lost Rac/The Lost Racoon 2.0/Assets/Scripts/Arthur Scripts/Player/Movement/PlayerController.cs
@@ -9,6 +9,7 @@ public class PlayerController : MonoBehaviour {
     //Input
     public PlayerInput playerInput;
     public InputAction moveAction;
+    public Transform orientation;
     // Update is called once per frame
     private void Start() {
         moveAction = playerInput.actions["WASD"];
@@ -18,11 +19,12 @@ public class PlayerController : MonoBehaviour {
         //Movement
         Vector3 move = new Vector3();
         Vector2 input = moveAction.ReadValue<Vector2>();
-        move = new Vector3(input.x, 0, input.y).normalized;
+        Vector3 inputDir = orientation.forward * -input.x + orientation.right * input.y;
+        //move = new Vector3(input.x, 0, input.y).normalized;
         float v = new float();
         float h = new float();
         float j = new float();
 
-        transform.Translate(move * Time.deltaTime * speed);
+        transform.Translate(inputDir.normalized * Time.deltaTime * speed);
     }
 }
--- The Lost Rac/The Lost Racoon 2.0/Assets/Scripts/Arthur Scripts/Player/Movement/ThirdPersonCam.cs
+++ The Lost Rac/The Lost Racoon 2.0/Assets/Scripts/Arthur Scripts/Player/Movement/ThirdPersonCam.cs
@@ -6,27 +6,33 @@ using UnityEngine.InputSystem;
 public class ThirdPersonCam : MonoBehaviour
 {
     [Header("References", order = 0)]
-    public Transform orientation;
-    public Transform player;
-    public Transform playerObj;
-    public Rigidbody rb;
     public PlayerInput playerInput;
     public InputAction moveAction;
+    public CharacterController controller;
+    public Transform cam;
     [Header("Debug", order = 1)]
     public float rotationSpeed;
+    public int speed;
+    float turnSmoothVelocity;
 
     private void Start() {
+        moveAction = playerInput.actions["WASD"];
         Cursor.lockState = CursorLockMode.Locked;
         Cursor.visible = false;
     }
     private void Update() {
-        Vector3 viewDir = player.position - new Vector3(transform.position.x, transform.position.y, transform.position.z);
-        orientation.forward = viewDir.normalized;
+        //Vector2 input = moveAction.ReadValue<Vector2>();
+        float horizontal = Input.GetAxisRaw("Horizontal");
+        float vertical = Input.GetAxisRaw("Vertical");
+        Vector3 direction = new Vector3(horizontal, 0f, vertical).normalized;
 
-        Vector2 input = moveAction.ReadValue<Vector2>();
-        Vector3 inputDir = orientation.forward * input.x + orientation.right * input.y;
-        if(inputDir != Vector3.zero) {
-            playerObj.forward = Vector3.Slerp(playerObj.forward, inputDir.normalized, Time.deltaTime * rotationSpeed);
+        if(direction.magnitude >= 0.1f) {
+            float targetAngle = Mathf.Atan2(direction.x, direction.z) * Mathf.Rad2Deg + cam.eulerAngles.y;
+            float angle = Mathf.SmoothDampAngle(transform.eulerAngles.y, targetAngle, ref turnSmoothVelocity, 0.1f);
+            transform.rotation = Quaternion.Euler(0f, angle, 0f);
+
+            Vector3 moveDir = Quaternion.Euler(0f, targetAngle, 0f) * Vector3.forward;
+            controller.Move(moveDir.normalized * speed * Time.deltaTime);
         }
     }
 }
