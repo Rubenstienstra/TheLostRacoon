10986cffd2aef13f051335f0fc306cc2ae13ffa8
arthur-lava <97080076@st.deltion.nl>
2022-09-09 14:10:57 +0200

    camera verbeterd en sprint toe gevoegd

--- The Lost Racoon 2.0/Assets/Scripts/Arthur Scripts/Player/Movement/ThirdPersonCam.cs
+++ The Lost Racoon 2.0/Assets/Scripts/Arthur Scripts/Player/Movement/ThirdPersonCam.cs
@@ -8,31 +8,40 @@ public class ThirdPersonCam : MonoBehaviour
     [Header("References", order = 0)]
     public PlayerInput playerInput;
     public InputAction moveAction;
+    public InputAction sprintAction;
     public CharacterController controller;
     public Transform cam;
     [Header("Debug", order = 1)]
-    public float rotationSpeed;
     public int speed;
+    public int sprintSpeed;
     float turnSmoothVelocity;
 
     private void Start() {
         moveAction = playerInput.actions["WASD"];
+        sprintAction = playerInput.actions["Sprint"];
         Cursor.lockState = CursorLockMode.Locked;
         Cursor.visible = false;
     }
     private void Update() {
-        //Vector2 input = moveAction.ReadValue<Vector2>();
-        float horizontal = Input.GetAxisRaw("Horizontal");
-        float vertical = Input.GetAxisRaw("Vertical");
-        Vector3 direction = new Vector3(horizontal, 0f, vertical).normalized;
+        controller.transform.position = transform.position;
+        Vector2 input = moveAction.ReadValue<Vector2>();
+        
+        Vector3 direction = new Vector3(input.x, 0f, input.y).normalized;
 
+        //Sprint
+        float sprint = sprintAction.ReadValue<float>();
+        
         if(direction.magnitude >= 0.1f) {
             float targetAngle = Mathf.Atan2(direction.x, direction.z) * Mathf.Rad2Deg + cam.eulerAngles.y;
             float angle = Mathf.SmoothDampAngle(transform.eulerAngles.y, targetAngle, ref turnSmoothVelocity, 0.1f);
             transform.rotation = Quaternion.Euler(0f, angle, 0f);
 
             Vector3 moveDir = Quaternion.Euler(0f, targetAngle, 0f) * Vector3.forward;
-            controller.Move(moveDir.normalized * speed * Time.deltaTime);
+            if (sprint == 0) {
+                controller.Move(moveDir.normalized * speed * Time.deltaTime);
+            } else {
+                controller.Move(moveDir.normalized * sprintSpeed * Time.deltaTime);
+            }
         }
     }
 }
