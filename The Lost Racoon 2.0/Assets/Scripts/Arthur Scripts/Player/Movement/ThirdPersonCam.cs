de2d0b94edb68ad0219e6c49177489ca0839d63c
arthur-lava <97080076@st.deltion.nl>
2022-09-09 16:52:13 +0200

    charged jump is done

--- The Lost Racoon 2.0/Assets/Scripts/Arthur Scripts/Player/Movement/ThirdPersonCam.cs
+++ The Lost Racoon 2.0/Assets/Scripts/Arthur Scripts/Player/Movement/ThirdPersonCam.cs
@@ -7,41 +7,65 @@ public class ThirdPersonCam : MonoBehaviour
 {
     [Header("References", order = 0)]
     public PlayerInput playerInput;
-    public InputAction moveAction;
-    public InputAction sprintAction;
+    public InputAction moveAction, sprintAction, jumpAction;
     public CharacterController controller;
     public Transform cam;
-    [Header("Debug", order = 1)]
-    public int speed;
-    public int sprintSpeed;
+    public Rigidbody rb;
+    [Header("Changeable", order = 1)]
+    public float speed;
+    public float sprintSpeed;
+    public float jumpCharge, jumpMin, jumpMax, chargePerSec;
+    [Header("Debug", order = 2)]
     float turnSmoothVelocity;
+    bool isGrounded;
+    bool charged;
+    Vector3 jump;
 
     private void Start() {
         moveAction = playerInput.actions["WASD"];
         sprintAction = playerInput.actions["Sprint"];
+        jumpAction = playerInput.actions["Jump"];
         Cursor.lockState = CursorLockMode.Locked;
         Cursor.visible = false;
+        jump = new Vector3(0.0f, 0.2f, 0.0f);
     }
     private void Update() {
+        //input
         Vector2 input = moveAction.ReadValue<Vector2>();
-        
         Vector3 direction = new Vector3(input.x, 0f, input.y).normalized;
+        float sprintInput = sprintAction.ReadValue<float>();
+        float jumpInput = jumpAction.ReadValue<float>();
 
-        //Sprint
-        float sprint = sprintAction.ReadValue<float>();
-        
-        if(direction.magnitude >= 0.1f) {
+        //Movement
+        if (direction.magnitude >= 0.1f) {
             float targetAngle = Mathf.Atan2(direction.x, direction.z) * Mathf.Rad2Deg + cam.eulerAngles.y;
             float angle = Mathf.SmoothDampAngle(transform.eulerAngles.y, targetAngle, ref turnSmoothVelocity, 0.1f);
             transform.rotation = Quaternion.Euler(0f, angle, 0f);
 
             Vector3 moveDir = Quaternion.Euler(0f, targetAngle, 0f) * Vector3.forward;
-            if (sprint == 0) {
+            if (sprintInput == 0) {
                 controller.Move(moveDir.normalized * speed * Time.deltaTime);
-            } else {
+            } else { //Sprint
                 controller.Move(moveDir.normalized * sprintSpeed * Time.deltaTime);
             }
         }
-        controller.transform.position = transform.position;
+        //controller.transform.position = transform.position;
+        if (jumpInput == 1 && isGrounded == true) {
+            if (jumpCharge <= jumpMax) {
+                jumpCharge += chargePerSec * Time.deltaTime;
+            }
+            charged = true;
+        }
+        if (jumpInput == 0 && charged == true) {
+            rb.AddForce(jump * jumpCharge, ForceMode.Impulse);
+            jumpCharge = jumpMin;
+            charged = false;
+            isGrounded = false;
+        }
+    }
+    private void OnCollisionEnter(Collision collision) {
+        if(collision.gameObject.tag == "Ground") {
+            isGrounded = true;
+        }
     }
 }
