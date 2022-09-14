f58c38eb31f553dc4aa42d1dd6cc17dcd348b813
arthur-lava <97080076@st.deltion.nl>
2022-09-14 15:50:55 +0200

    cinametic cam position freeze work in progress

--- The Lost Racoon 2.0/Assets/Scripts/Arthur Scripts/Player/PickUp/Pickup.cs
+++ The Lost Racoon 2.0/Assets/Scripts/Arthur Scripts/Player/PickUp/Pickup.cs
@@ -1,15 +1,77 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.InputSystem;
 
 public class Pickup : MonoBehaviour
 {
+    [Header("Debug", order = 0)]
+    public float pickUpRange;
+    public float dropForwardForce, dropUpwardForce;
+
+    [Header("References", order = 1)]
+    public PlayerInput playerInput;
+
+    [Header("References", order = 2)]
+    public Rigidbody rb;
+    public Rigidbody playerRB;
+    public BoxCollider coll;
+    public Transform mouth, itemContainer, player;
+
+    public bool equipped;
+    public bool slotfull;
 
     private void Start() {
-        
+        if (!equipped) {
+            rb.isKinematic = false;
+            coll.isTrigger = false;
+        }
+        if (equipped) {
+            rb.isKinematic = true;
+            coll.isTrigger = true;
+            slotfull = true;
+        }
     }
-    void Update()
-    {
-        
+    private void Update() {
+        //input
+        float interactInput = playerInput.actions["Interact"].ReadValue<float>();
+        float dropInput = playerInput.actions["Drop"].ReadValue<float>();
+        //Check if item is near hand and player pressed interaction key
+        Vector3 distanceToPaw = mouth.position - transform.position;
+        if(!equipped && distanceToPaw.magnitude <= pickUpRange && interactInput == 1 && !slotfull) {
+            PickUp();
+        }
+        if(equipped && dropInput == 1) {
+            Drop();
+        }
+    }
+
+    private void PickUp() {
+        equipped = true;
+        slotfull = true;
+
+        transform.SetParent(itemContainer);
+        transform.localPosition = Vector3.zero;
+        transform.localRotation = Quaternion.Euler(Vector3.zero);
+        transform.localScale = Vector3.one;
+
+        rb.isKinematic = true;
+        coll.isTrigger = true;
+    }
+
+    private void Drop() {
+        equipped = false;
+        slotfull = false;
+
+        transform.SetParent(null);
+
+        rb.isKinematic = false;
+        coll.isTrigger = false;
+
+        rb.velocity = playerRB.velocity;
+
+        //Add dropforce
+        rb.AddForce(player.forward * dropForwardForce, ForceMode.Impulse);
+        rb.AddForce(player.forward * dropUpwardForce, ForceMode.Impulse);
     }
 }
