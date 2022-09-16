db04a3d2f737932967f69035bf704b70344e15fd
arthur-lava <97080076@st.deltion.nl>
2022-09-16 15:52:44 +0200

    interaction verbetert

--- The Lost Racoon 2.0/Assets/Scripts/Arthur Scripts/Player/Interact/Interact.cs
+++ The Lost Racoon 2.0/Assets/Scripts/Arthur Scripts/Player/Interact/Interact.cs
@@ -5,18 +5,47 @@ using UnityEngine.InputSystem;
 
 public class Interact : MonoBehaviour
 {
+    [Header("References", order = 0)]
     public PlayerInput playerInput;
-    public InputAction interact;
-
-    // Update is called once per frame
+    [Header("Detection Sphere", order = 1)]
+    public Transform detectionAria;
+    public float detectDiameter;
+    [Header("Interaction Sphere", order = 1)]
+    public Transform interactionAria;
+    public float interactionDiameter;
+    bool interacted;
+    //piemel -davido 
     private void Start() {
         
     }
     void Update()
     {
         float interactInput = playerInput.actions["Interact"].ReadValue<float>();
-        if (interactInput == 1) {
-            //intraction here
+        Collider[] detectedColliders = Physics.OverlapSphere(detectionAria.position, detectDiameter * 2);
+        foreach (Collider coll in detectedColliders) {
+            if(coll.gameObject.tag == "interactible") {
+                Debug.Log("Interactible nearby");
+                //Ui elliment stage 1
+            }
         }
+        Collider[] interactionColliders = Physics.OverlapSphere(interactionAria.position, interactionDiameter * 2);
+        
+        foreach (Collider coll in interactionColliders) {
+            if (coll.gameObject.tag == "interactible") {
+                Debug.Log("Interactible in reach, press E to interact");
+                //Ui elliment stage 2
+                if (interactInput == 1) {
+                    for (int i = 0; i < interactionColliders.Length; i++) {
+                        //
+                    }
+                }
+            }
+        }
+    }
+    private void OnDrawGizmos() {
+        Gizmos.color = Color.red;
+        Gizmos.DrawWireSphere(detectionAria.position, detectDiameter * 2);
+        Gizmos.color = Color.green;
+        Gizmos.DrawWireSphere(interactionAria.position, interactionDiameter * 2);
     }
 }
