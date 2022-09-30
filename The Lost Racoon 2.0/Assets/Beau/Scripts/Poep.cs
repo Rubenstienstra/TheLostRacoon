b06600b42e0e9e3f6a0caac38e69a870ce986f27
Beau00 <70371863+Beau00@users.noreply.github.com>
2022-09-30 15:52:29 +0200

    peo[p

---
+++ The Lost Racoon 2.0/Assets/Beau/Scripts/Poep.cs
@@ -0,0 +1,12 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class Poep : MonoBehaviour
+{
+    public void ButtonNewGaame()
+    {
+        SceneManager.LoadScene(1);
+    }
+}
