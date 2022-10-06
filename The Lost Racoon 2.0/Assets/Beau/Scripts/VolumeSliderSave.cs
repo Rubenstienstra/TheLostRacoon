8c108ff5398404f3db61e0b3fdc47a01eedcc15a
Beau00 <70371863+Beau00@users.noreply.github.com>
2022-10-06 15:12:30 +0200

    changens esc

--- The Lost Racoon 2.0/Assets/Beau/Scripts/VolumeSliderSave.cs
+++ The Lost Racoon 2.0/Assets/Beau/Scripts/VolumeSliderSave.cs
@@ -8,10 +8,15 @@ public class VolumeSliderSave : MonoBehaviour
     public AudioMixer sfxSlider;
     public AudioMixer musicSlide;
     public ScriptableSaving scriptableInfo;
-    
+    float music;
+    float sfx;
     public void VolumeSlider()
     {
+        musicSlide.GetFloat("volume", out music);
+        scriptableInfo.volumeMUSIC = music;
 
+        sfxSlider.GetFloat("volume", out sfx);
+        scriptableInfo.volumeSFX = sfx;
     }
 
 }
